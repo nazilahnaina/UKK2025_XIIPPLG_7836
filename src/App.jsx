7baa6de6e2ba4220ab0f3141973e729ee93f3402
nazilahnaina <143465114+nazilahnaina@users.noreply.github.com>
2025-02-13 10:52:22 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import { Typography } from '@mui/material'
+
+export default function App() {
+
+  return (
+    <>
+        <Typography variant='h2'>UKK2025_XIIPPLG2_7836</Typography>
+    </>
+
+    
+  )
+}
---
+++ src/theme/color.jsx
@@ -0,0 +1,30 @@
+export const indigo = {
+    50: "#e8eaf6",
+    100: "##c5cae9",
+    200: "##9fa8da",
+    300: "##7986cb",
+    400: "##5c6bc0",
+    500: "##3f51b5",
+    600: "##3949ab",
+    700: "##303f9f",
+    800: "##283593",
+    900: "##1a237e",
+  };
+  export const dark = {
+    100: "#E9E9E9",
+    200: "#D3D3D3",
+    300: "#1E1E1E",
+    400: "#696262",
+    500: "#1E2126",
+    600: "#D9D9D9",
+  };
+  export const light = {
+    100: "#FFFFFF",
+    200: "#F4F4F4",
+  };
+  export const succes = {
+    100: "#6FE29D",
+  };
+  export const danger = {
+    100: "#F56767",
+  };
